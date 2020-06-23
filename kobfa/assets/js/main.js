9b76caf7fae94fff054008922edaab61f06a9233
kondear <kondear@exelt.net>
2020-06-23 14:45:27 +0700

    Change language en / thai

--- kobfa/assets/js/main.js
+++ kobfa/assets/js/main.js
@@ -142,3 +142,15 @@ window.onscroll = function() {
 $(".close-toggle").click(function() {
     $(".cards-modal").hide();
 });
+
+// Change language en/thai
+var document = document;
+
+function changeLanguage(elClassName) {
+    const showClassName = elClassName
+    const hideClassName = elClassName == 'en' ? 'th' : 'en'
+    $('.' + showClassName).show()
+    $('.' + hideClassName).hide()
+}
+
+changeLanguage('th')
