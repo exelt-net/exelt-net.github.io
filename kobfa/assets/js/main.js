525eb98f33c50858d06a796a556adf9c7628880e
kondear <kondear@exelt.net>
2020-06-28 00:08:46 +0700

    add curved cut anime

--- kobfa/assets/js/main.js
+++ kobfa/assets/js/main.js
@@ -71,7 +71,11 @@ $(document).ready(function() {
 
     // click tab
     $("#motion-autoplay #slide-menu a").click((e) => {
-        $('.video').load();
+        // $('.video').load();
+
+        $('.video').each((k, el) => {
+            $(el)[0].currentTime = 0
+        })
 
         const slideMenus = $("#motion-autoplay #slide-menu li");
         slideMenus.filter(".selected").removeClass("selected")
@@ -147,3 +151,19 @@ function changeLanguage(elClassName) {
 }
 
 changeLanguage('en')
+
+
+// 
+function inViewport($el) {
+    var H = $(window).height(),
+        r = $el[0].getBoundingClientRect(),
+        t = r.top,
+        b = r.bottom;
+    return Math.max(0, t > 0 ? H - t : (b < H ? b : H));
+}
+
+$(window).on("scroll resize", function() {
+    var window_offset = inViewport($('.curved-cut-block'));
+    $(".overlay").height(window_offset);
+    // $(".caption").css("bottom", (window_offset / 4));
+});
