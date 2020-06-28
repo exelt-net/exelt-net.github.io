c312923b12f394b7a73329d68a1e63340cf5db33
kondear <kondear@exelt.net>
2020-06-28 19:11:56 +0700

    test video

--- kobfa/assets/js/main.js
+++ kobfa/assets/js/main.js
@@ -56,35 +56,35 @@ $(document).ready(function() {
     })
 
     // Video motion
-    // function slideShow(idx = 0) {
-    //     const speed = 500;
+    function slideShow(idx = 0) {
+        const speed = 500;
 
-    //     const slides = $("#motion-autoplay #slide-content li");
-    //     const targetSlide = $(slides[idx]);
+        const slides = $("#motion-autoplay #slide-content li");
+        const targetSlide = $(slides[idx]);
 
-    //     slides.filter(".active").stop(true, false).fadeOut(speed, () => {
-    //         $(this).removeClass("active");
-    //     });
-    //     targetSlide.stop(true, false).fadeIn(speed).addClass("active");
-    // }
+        slides.filter(".active").stop(true, false).fadeOut(speed, () => {
+            $(this).removeClass("active");
+        });
+        targetSlide.stop(true, false).fadeIn(speed).addClass("active");
+    }
 
     // click tab
-    // $("#motion-autoplay #slide-menu a").click((e) => {
-    //     // $('.video').load();
+    $("#motion-autoplay #slide-menu a").click((e) => {
+        // $('.video').load();
 
-    //     $('.video-start').each((k, el) => {
-    //         $(el)[0].currentTime = 0
-    //     })
+        $('.video-start').each((k, el) => {
+            $(el)[0].currentTime = 0
+        })
 
-    //     const slideMenus = $("#motion-autoplay #slide-menu li");
-    //     slideMenus.filter(".selected").removeClass("selected")
+        const slideMenus = $("#motion-autoplay #slide-menu li");
+        slideMenus.filter(".selected").removeClass("selected")
 
-    //     const a = $(e.currentTarget)
-    //     a.parent().addClass("selected")
+        const a = $(e.currentTarget)
+        a.parent().addClass("selected")
 
-    //     const idx = parseInt(a.attr('data-key'))
-    //     slideShow(idx);
-    // });
+        const idx = parseInt(a.attr('data-key'))
+        slideShow(idx);
+    });
 });
 
 // slide cover image
