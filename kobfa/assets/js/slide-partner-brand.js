338a2778d90f37a763fe1add13f5ebaa6fc87b15
kondear <kondear@exelt.net>
2020-06-29 11:01:59 +0700

    player pause fuction

--- kobfa/assets/js/slide-partner-brand.js
+++ kobfa/assets/js/slide-partner-brand.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
     function changeSlide(carousel, key = 0) {
         const instance = M.Carousel.getInstance(carousel);
         instance.set(key)
+        $(".player").each(function() { this.pause() });
     }
 
     function nextSlide(carousel, num = 1) {
         const instance = M.Carousel.getInstance(carousel);
         instance.next(num)
+
     }
 
     function prevSlide(carousel, num = 1) {
@@ -30,12 +32,13 @@ $(document).ready(function() {
             changeSlide(carouselA, key)
         } catch (e) {}
     }
-
+    $(".player").each(function() { this.pause() });
     const carouselA = $("#carousel-review").carousel({ onCycleTo: onSlideA });
     const carouselB = $("#carousel-brand").carousel({ onCycleTo: onSlideB });
 
     $('#next').click(() => {
         nextSlide(carouselA)
+
     })
 
     $('#prev').click(() => {
