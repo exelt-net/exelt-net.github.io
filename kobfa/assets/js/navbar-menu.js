38136d66e3534f1f2af85bedc8c45598697d9328
kondear <kondear@exelt.net>
2020-08-05 11:01:03 +0700

    edit style menu navbar

--- kobfa/assets/js/navbar-menu.js
+++ kobfa/assets/js/navbar-menu.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown();
 });
+
+
 // Hide Menu on Scroll
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("hide-menu-scroll").style.top = "0";
-    } else {
-        document.getElementById("hide-menu-scroll").style.top = "-88px";
-    }
-    prevScrollpos = currentScrollPos;
-}
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("hide-menu-scroll").style.top = "0";
+//     } else {
+//         document.getElementById("hide-menu-scroll").style.top = "-88px";
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
