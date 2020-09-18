ce62d2dfd28292ddeeef29742ca1d77d2a0e0cd4
kondear-exelt <65880748+kondear-exelt@users.noreply.github.com>
2020-09-18 17:34:52 +0700

    add localStorage() change language of function

---
+++ kobfa/assets/js/data-language.js
@@ -0,0 +1,162 @@
+
+var data = {
+  en: {
+    home_title: 'Home',
+    clients_partners_title: 'Clients & partners',
+    news_project_title: 'News & Projects',
+    org_title: 'Organizations',
+    school_title: 'Schools',
+    individuals_title: 'Individuals',
+    follow_title: 'Follow us on',
+    address_company: 'Address : 208/1 Moo 1, Pa Daet Sub-district, Mueang Chiang Mai District, Chiang Mai 50100',
+    phone_company: 'Tel:089-xxxxxxxx ',
+    email_company: 'Email:support@exelt.net',
+
+    // modal content
+    contact_title: 'Contact us',
+    contact_des: 'You can send us a simple message here. You can tell us a little bit about your needs',
+    send_message_button: 'send message',
+    sent_message_button:'sent message',
+    
+   // slide cover images
+    world_class_title: 'World class',
+    world_class_des: 'language learning software',
+    consultants_title: 'International Standard',
+    consultants_des: 'Consultants',
+    unskill_title: 'Upskill your workforce',
+    upgrade_program_title: 'Upgrade your',
+    upgrade_program_des: 'English Program',
+    give_your_exexutives_title: 'people the tools to succeed',
+    give_your_exexutives_des: 'Give your executives',
+    language_learning_title: 'Language learning system that suits all.',
+    language_learning_des: 'At KOBFA we use the most advanced language learning software complemented by expert consultancy and a deep understanding of Thai culture. We provide a complete solution for any organization looking to upskill its members or provide a high quality, cost-effective English program.',
+    
+   // How we work
+   how_we_work_title: 'How We work',
+   how_we_work_title1: '1. Needs Analysis',
+   how_we_work_des1: 'We Find Out Your Needs',
+   how_we_work_title2: '2. Course Designs',
+   how_we_work_des2: 'We Build A Course To Meet Your Needs',
+   how_we_work_title3: '3. Course Delivery',
+   how_we_work_des3: 'The Best Software, The Best Consultants',
+   how_we_work_title4: '4. Real Evidence Of Learning',
+   how_we_work_des4: 'etailed Reports With Video And Audio Files',
+
+   exelt_button: 'powered by',
+   
+   // Our services
+   our_service_title: 'Our Services',
+   our_service_dep: 'We provide a range of solutions to meet the needs of Thai schools, organizations, students and individuals needs. Through our world-class LMS (Learning Management System) and team of experienced writers we are able to produce courses of any length and content.',
+   our_service_type_title1: 'ORGANIZATION',
+   our_service_type_des1: 'COURSE PLAN',
+   our_service_type_title2: 'INDIVIDUAL',
+   our_service_type_des2: 'STUDY',
+   our_service_type_title3: 'SCHOOL',
+   our_service_type_des3: 'STUDY',
+
+  // About us
+  about_title: 'About us',
+  about_des: 'Our tech team is made up of highly experienced programmers and designers. Our team of consultants is made up of some of the most experienced teachers, teacher trainers and writers working in the industry today. Our consultants have an average of over 25 years’ industry experience and have written and and delivered courses for organizations such as Cambridge Esol, International House and the British Council.',
+  find_out_more: 'Find out more',
+
+  // Panal bar
+  analysis_title: 'Begin your course today',
+  analysis_button: 'Analyse what You need',
+  
+  // Review and customer
+  reviews_title: 'REVIEWS FROM CUSTOMERS',
+
+   // News and Projects
+   news_topic1: 'Levelling Up projects',
+   news_des1: 'The project is designed to enable schools and regions to achieve high quality language teaching despite having minimal resources.',
+},
+
+  th: {
+    home_title: 'หน้าแรก',
+    clients_partners_title:'ลูกค้าและพันธมิตร',
+    news_project_title: 'ข่าวสารและโครงการต่าง',
+    org_title: 'องค์กร',
+    school_title: 'โรงเรียน',
+    individuals_title: 'บุคคลทั่วไป',
+    upgrade_program_title: 'ยกระดับ',
+    upgrade_program_des: 'โปรแกรมภาษาอังกฤษของคุณ',
+    follow_title: 'ติดตามได้ที่',
+    address_company: 'ที่อยู่ : 208/1 Moo 1, Pa Daet Sub-district, Mueang Chiang Mai District, Chiang Mai 50100',
+    phone_company: 'โทร:089-xxxxxxxx ',
+    email_company: 'อีเมล:support@exelt.net',
+
+    // modal content(th)
+    contact_title: 'ติดต่อเรา',
+    contact_des: 'คุณสามารถส่งข้อความง่ายๆได้ที่นี่ บอกให้เราทราบความต้องการของคุณสักเล็กน้อย',
+    send_message_button: 'ส่งข้อความ',
+    sent_message_button:'ส่งข้อความแล้ว',
+
+    // slide cover images (th)
+    world_class_title: 'ซอฟต์แวร์',
+    world_class_des: 'การเรียนภาษาชั้นนำ',
+    consultants_title: 'ที่ปรึกษา',
+    consultants_des: 'มาตรฐานสากล',
+    unskill_title: 'เพิ่มทักษะให้ทีมงานของคุณ',
+    give_your_exexutives_title: 'ส่งต่อให้กับผู้บริหารหรือทีมงานของคุณ',
+    give_your_exexutives_des: 'รับเครื่องมือเพื่อความสำเร็จ',
+    language_learning_title: 'เราทำงานอย่างไร',
+    language_learning_des: 'ที่ขอบฟ้าเราใช้ซอฟต์แวร์การเรียนภาษาที่ทันสมัยที่สุดซึ่งเติมเต็มด้วยการให้คำปรึกษาอย่างเชี่ยวชาญรวมไปถึงการเข้าใจวัฒนธรรมไทยอย่างถ่องแท้เรามีวิธีแก้ปัญหาอย่างครบถ้วนสำหรับองค์กรที่กำลังมองหาการเพิ่มทักษะให้กับทีมงานหรือทำโปรแกรมภาษาอังกฤษที่มีคุณภาพสูงแต่คุ้มค่า',
+ 
+    // How we work (th)
+    how_we_work_title: 'เราทำงานอย่างไร',
+    how_we_work_title1: '1. แบบวิเคราะห์ความต้องการ',
+    how_we_work_des1: 'เราจะหาความต้องการของคุณ',
+    how_we_work_title2: '2. การออกแบบคอร์ส',
+    how_we_work_des2: 'เราสร้างคอร์สที่ตรงกับความต้องการของคุณ',
+    how_we_work_title3: '3. การส่งมอบคอร์ส',
+    how_we_work_des3: 'ซอฟต์แวร์ที่ดีที่สุด ที่ปรึกษาที่ดีที่สุด',
+    how_we_work_title4: '4. พิสูจน์ผลลัพธ์ได้จริง',
+    how_we_work_des4: 'ด้วยรายงานผลการเรียนอย่างละเอียด พร้อมทั้งไฟล์วิดีโอและเสียง',
+    exelt_button: 'powered by',
+
+    // Our services (th)
+    our_service_title: 'บริการของเรา',
+    our_service_dep: 'เราให้บริการแก้ปัญหาแบบหลากหลายวิธีเพื่อให้ตรงตามความต้องการของโรงเรียน องค์กร นักเรียน หรือที่บุคคลทั่วไปต้องการ ด้วย LMS (Learning Management System) ระดับสากลและทีมผู้เขียนที่มากประสบการณ์ของเราทำให้เราสามารถสร้างคอร์สที่มีความยาวและเนื้อหาเท่าใดก็ได้',
+    our_service_type_title1: 'วางแผน',
+    our_service_type_des1: 'ขององค์กร',
+    our_service_type_title2: 'การเรียน',
+    our_service_type_des2: 'บุคคลทั่วไป',
+    our_service_type_title3: 'เรียนรู้',
+    our_service_type_des3: 'ในโรงเรียน',
+
+    // About us (th)
+    about_title: 'เกี่ยวกับเรา',
+    about_des: 'ทีมเทคโนโลยีของเราประกอบด้วยโปรแกรมเมอร์และนักออกแบบที่มีประสบการณ์สูง ทีมที่ปรึกษาของเราประกอบไปด้วยครูจากกลุ่มครูที่มีประสบการณ์มากที่สุด ผู้ฝึกอบรมครู และนักเขียนที่ทำงานอยู่ในอุตสาหกรรมการเรียนการสอนภาษาอังกฤษในปัจจุบัน ที่ปรึกษาของเรามีประสบการณ์ในอุตสาหกรรมการเรียนการสอนภาษาอังกฤษกว่า 25 ปี และได้เขียนและจัดทำหลักสูตรสำหรับองค์กรต่าง ๆ ตัวอย่างเช่น Cambridge English, International House, Trinity Colledge London และ British Council',
+    find_out_more: 'ข้อมูลเพิ่มเติม',
+
+    // Panal bar(th)
+    analysis_title: 'เริ่มเปิดคอร์สได้ตั้งแต่วันนี้',
+    analysis_button: 'วิเคราะห์ความต้องการของคุณ',
+
+
+    // Review and Customers(th)
+    reviews_title: 'เคยร่วมงานกับเรา',
+
+    // News and Projects(th)
+    news_topic1: 'โครงการ “Levelling Up”',
+    news_des1: 'โครงการนี้ถูกออกแบบมาเพื่อทำให้โรงเรียนสามารถจัดการเรียนการสอนคุณภาพสูงได้แม้จะมีทรัพยากรเพียงเล็กน้อย',
+  },
+}
+
+let lang = localStorage.getItem('lang') || 'en'
+chageLang(lang)
+
+function chageLang(lang = 'en') {
+  String.i18n(document, data[lang])
+  localStorage.setItem('lang', lang)
+
+  if (lang == 'en') {
+    $('#btn-en').addClass('active')
+    $('#btn-th').removeClass('active')
+  }
+
+  if (lang == 'th') {
+    $('#btn-en').removeClass('active')
+    $('#btn-th').addClass('active')
+  }
+}
