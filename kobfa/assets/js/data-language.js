dc282710ff52bad3e6a0ff292ad2ab638af6b6d5
kondear-exelt <65880748+kondear-exelt@users.noreply.github.com>
2020-09-20 16:06:36 +0700

    set language(en/th) in school page

--- kobfa/assets/js/data-language.js
+++ kobfa/assets/js/data-language.js
@@ -11,6 +11,7 @@ var data = {
     address_company: 'Address : 208/1 Moo 1, Pa Daet Sub-district, Mueang Chiang Mai District, Chiang Mai 50100',
     phone_company: 'Tel:089-xxxxxxxx ',
     email_company: 'Email:support@exelt.net',
+    we_found_out: 'What we’ve found out...',
 
     // modal content
     contact_title: 'Contact us',
@@ -72,7 +73,7 @@ var data = {
 
   //  Organization page
   org_intro: 'At KOBFA we can design courses to meet the language learning needs of any company or organization.',
-  org_recomment_title: 'What we’ve found out...',
+  
   org_recomment_des: 'Times are hard and competition is fierce. We need to show a high level of professionalism in  everything we do. It will save us money if we can develop and promote from within. Our staff need better English, but we don’t have the time or money to send them on training courses or long study courses.',
   org_composition_title1: 'Tailored courses',
   org_composition_des1: 'At KOBFA we design tailored courses for organizations and because of this we can not only help staff with their English but also combine this with training in the exact information and methods the organization wishes them to follow.',
@@ -82,6 +83,51 @@ var data = {
   org_composition_des3: 'In addition, our software allows staff to study on their own devices anywhere, meaning that they can continue to develop in their free time and even in downtime on the job they can be using their time productively.',
   org_composition_title4: 'Anytime, Anywhere',
   org_composition_des4: 'Our consultants are available to supplement self-study with face to face or online sessions, all  done at a time that suits you and your staff. This self-study aspects allows us to minimize the time needed with our consultants, thereby saving you time and money!',
+
+  // Schools page
+  school_intro: 'We spent years listening...',
+  school_issues_heading: 'School issues',
+  study_tab_heading: 'Online-Offline or Blended',
+  study_tab_heading_mo: 'Online-Offline',
+  study_tab_title1: 'Flexible classrooms',
+  study_tab_des1: 'We provide engaging and effective distance learning in one, easy to use platform for any device. Our system is entirely flexible and can be quickly adjusted to provide whatever balance of classroom time and home study your school needs, meaning you stay ahead of any situation.',
+  study_tab_title2: 'We need a simple way to keep track of progress',
+  study_tab_des2: 'Online study has automated marking to save teachers time. In addition,  students can upload video or audio recordings and written work to show evidence of their ability. Pic Our report system is easy to use and  our  consultants are available to help with  course delivery, assessment and reports.',
+  study_tab_teacher_review_title1: '"We need a simple way to keep track of progress"',
+  study_tab_teacher_review_des1: '- Head teacher Lanna school -',
+  study_tab_teacher_review_title2: 'We need a better solution for online earning. Lockdown was difficult/Don t know when it will happen again',
+  study_tab_teacher_review_des2: '- Head teacher Lanna school -',
+  // 
+  classroom_tab_heading: 'Upgrade Classroom',
+  classroom_tab_heading_mo: 'Classroom',
+  classroom_teacher_review_title1: '"We want to get the best English program but we need value for money.”',
+  classroom_teacher_review_des1: '- Head teacher Lanna school -',
+  classroom_tab_des: 'Our software allows us to write bespoke courses to fit your school’s curriculum and culture, and to match your students’ needs and interests. Lesson content is brought to life through exciting video, audio and visuals. Real world speaking tasks, warm ups, kinesthetic and competitive activities are built into all our courses, meaning you get the maximum engagement with the minimum preparation time.  <br/><br/> Although our software fits smoothly into a well equipped classroom and students’ own hand-held devices, it can also be exploited effectively in situations with the most limited resources, even using a single device. <br/><br/> Inexpensive projectors can be used to bring our activities to the whole class. In addition, our writers have extensive experience working with limited resources and can help learners reach their language goals with only everyday objects that surround them.<br/><br/> To learn more about our resource-limited services, see our <strong>“Levelling Up”</strong> project page.',
+  // 
+  value_tab_heading: 'Value for Money',
+  value_teacher_review_title1: '"We want to get the best English program but we need value for money."',
+  value_teacher_review_des1: '- Head teacher Lanna school -',
+  value_tab_des: 'Significant savings can be made by removing the cost of endlessly buying new series and editions of course books and preparing paper-based tests. Our courses and tests are written to the exact requirements of your school and students and can be rolled out again and again with minimal extra cost. They can be adjusted at any time to any degree. <br/><br/> Further savings are made as our courses reduce the time teachers need to spend on preparation and marking, with fun activities built into all lessons, automated marking systems and easy to use reports.<br/><br/> The end result is a new level of quality - with digital courses and reports saved to any device - as well as increased efficiency and significant savings.',
+  // 
+  teaching_issues_heading: 'Teaching issues',
+  thai_tab_heading: 'Thai Teacher Development',
+  thai_tab_heading_mo: 'Thai Teacher',
+  thai_tab_teacher_review_title1: '"I would like to develop my own English"',
+  thai_tab_teacher_review_des1: '- Head teacher Lanna school -',
+  thai_tab_teacher_review_title2: '"I would like to learn more teaching techniquess"',
+  thai_tab_teacher_review_des2: '- Head teacher Lanna school -',
+  thai_tab_des: 'Our courses contain clear and easy to follow guidelines for Thai teachers. Our software provides help with pronunciation of all key language items, and all guides to setting up software and lessons come with video instruction and Thai translation.<br/><br/> Our software allows Thai teachers to develop their own accuracy and awareness of typical errors as they go. In addition, our consultants are highly experienced teacher trainers and are available to provide focused help, assessment and feedback, either face to face or online.<br/><br/> This approach enables Thai teachers to prove their ability to deliver English programs effectively on a unit-by-unit, lesson-by-lesson basis. With our help, they do not need to be advanced English speakers or rely on foreign teachers to provide a high-quality learning experience for their students, and they are able to become increasingly confident and independent with every course they deliver.',
+  // 
+  foreign_tab_heading: 'Foreign Teacher Development',
+  foreign_tab_heading_mo: 'Foreign Teacher',
+  foreign_tab_teacher_review_title1: '"We like to use native speaker teachers but it’s expensive."',
+  foreign_tab_teacher_review_des1: '- Head teacher Lanna school -',
+  foreign_tab_teacher_review_title2: '"We have quite a high turnover of foreign staff."',
+  foreign_tab_teacher_review_des2: '- Head teacher Lanna school -',
+  foreign_tab_teacher_review_title3: '"I’d like to find a way to get the right balance: foreign teachers speak better English but our Thai teachers understand the students. "',
+  foreign_tab_teacher_review_des3: '- Head teacher Lanna school -',
+  foreign_tab_des: 'We believe that exposure to native speaker teachers is highly beneficial but not essential at every stage of a lesson or study program, particularly if you have a strong and steadily improving team of Thai teachers.<br/><br/> We can cater for every kind of team teaching situation, including using native speakers online or in classrooms and only at the exact points they are needed during or after a lesson, such as modelling language, providing assessment and error correction. Our consultants are available to provide native speaker services themselves, but we can also to train your foreign teachers to maximize their efficiency.<br/><br/> This allows schools to experiment with different levels of exposure to native speakers according to their budget and availability, and presents the opportunity to provide an outstanding learning experience while also developing independence and making significant budget savings.',
+  // thai_tab_heading_mo: 'cccc',
 },
 
   th: {
@@ -97,6 +143,7 @@ var data = {
     address_company: 'ที่อยู่ : 208/1 Moo 1, Pa Daet Sub-district, Mueang Chiang Mai District, Chiang Mai 50100',
     phone_company: 'โทร:089-xxxxxxxx ',
     email_company: 'อีเมล:support@exelt.net',
+    we_found_out: 'เราได้พบว่า...',
 
     // modal content(th)
     contact_title: 'ติดต่อเรา',
@@ -156,7 +203,6 @@ var data = {
 
      //  Organization page(TH)
      org_intro: 'ที่ขอบฟ้า เราสามารถออกแบบหลักสูตรเพื่อตอบสนองความต้องการในการเรียนภาษาของกลุ่ม หรือองค์กร หรือบุคคลใดก็ได้',
-    org_recomment_title: 'เราได้พบว่า...',
     org_recomment_des: 'ช่วงนี้เป็นช่วงที่ยากลำบากและการแข่งขันก็รุนแรง เราจำเป็นต้องแสดงความเป็นมืออาชีพระดับสูงในทุกๆ อย่างที่เราทำ มันจะช่วยเราประหยัดเงินหากเราสามารถพัฒนาและส่งเสริมภายในองค์กรได้ พนักงานของเราต้องมีภาษาอังกฤษดีกว่านี  แต่เราไม่มีเวลาหรือเงินที่จะส่งพวกเขาไปฝึกอบรมหรือเรียนหลักสูตรระยะยาว',
     org_composition_title1: 'หลักสูตรที่ออกแบบเพื่อคุณโดยเฉพาะ',
     org_composition_des1: ' ที่ขอบฟ้า เราออกแบบหลักสูตรที่เฉพาะเจาะจงสำหรับแต่ละองค์กร ซึ่งไม่ใช่แค่เพียงช่วยพนักงานได้ฝึกใช้ภาษาอังกฤษได้เท่านั้น  แต่ยังเป็นการผสมผสานระหว่างการฝึกอบรมเนื้อหาที่ต้องการและตามวิธีการที่แต่ละองค์ต้องการให้พนักงานปฎิบัติตามได้อีกด้วย',
@@ -166,6 +212,51 @@ var data = {
     org_composition_des3: 'นอกจากนี้ซอฟต์แวร์ของเรายังช่วยให้พนักงานสามารถเรียนโดยใช้อุปกรณ์ของตนเองได้จากที่ใดก็ได ซึ่งหมายความว่าพวกเขาสามารถพัฒนาตัวเองได้ในเวลาว่างหรือในเวลาที่มีงานไม่มาก  ส่งเสริมให้พนักงานได้ใช้เวลาว่างได้อย่างมีประสิทธิผล',
     org_composition_title4: 'ทุกที่ ทุกเวลา',
     org_composition_des4: 'ที่ปรึกษาของเราพร้อมที่จะเสริมการเรียนรู้ด้วยตัวเองแบบตัวต่อตัวหรือแบบออนไลน ทั้งหมดสามารถทำได้ในเวลาที่เหมาะสมกับคุณหรือพนักงานของคุณ การเรียนรู้ด้วยตัวเองนี้ช่วยให้เราสามารถลดเวลากับที่ปรึกษาของเราได  ซึ่งจะช่วยให้คุณประหยัดทั้งเวลาและเงินของคุณ',
+  
+    // Schools page
+    school_intro: 'เราใช้เวลาหลายปีในการรับฟัง...',
+    school_issues_heading: 'ปัญหาของโรงเรียน',
+    study_tab_heading: 'เรียนออนไลน์หรือผสมผสาน',
+    study_tab_heading_mo: 'เรียนออนไลน์',
+    study_tab_title1: 'ห้องเรียนที่มีความยืดหยุ่น',
+    study_tab_des1: 'เราให้บริการการเรียนทางไกลที่น่าสนใจและมีประสิทธิภาพ  ในแพลตฟอร์มที่ใช้งานได้ง่ายสำหรับทุกอุปกรณ ระบบของเรามีความยืดหยุ่นสูง เพราะสามารถปรับเปลี่ยนได้อย่างรวดเร็วตามความต้องการของผู้เรียน เพื่อให้เกิดความสมดุลของเวลาเรียน ระหว่างการเรียนที่บ้าน หรือตามที่โรงเรียนของคุณต้องการ นั่นหมายความว่าคุณนำหน้าในทุก ๆ สถานการณ์',
+    study_tab_title2: 'เราต้องการวิธีในการติดตามผลความก้าวหน้าที่ไม่ซับซ้อน',
+    study_tab_des2: 'การเรียนออนไลน์มีการให้คะแนนอัตโนมัติเพื่อประหยัดเวลาของคร นอกจากนั้น  นักเรียนสามารถอัปโหลดวิดีโอหรือบันทึกเสียง และงานเขียนเพื่อเป็นหลักฐานแสดงความสามารถของพวกเขาได ระบบรายงานผลของเราใช้ง่ายและที่ปรึกษาของเราก็พร้อมที่จะช่วยดูแลการเริ่มหลักสูตร การประเมิน และการรายงานผล',
+    study_tab_teacher_review_title1: '"เราต้องการวิธีง่าย ๆ ในการติดตามความคืบหน้า  "',
+    study_tab_teacher_review_des1: '- ครูใหญ่โรงเรียนลานนา -',
+    study_tab_teacher_review_title2: 'เราอยากได้ทางแก้ปัญหาในการเรียนออนไลน์ที่ดีกว่านี้',
+    study_tab_teacher_review_des2: '- ครูใหญ่โรงเรียนลานนา -',
+    // 
+    classroom_tab_heading: 'ยกระดับการสอนในห้องเรียน',
+    classroom_tab_heading_mo: 'สอนในห้องเรียน',
+    classroom_teacher_review_title1: '"เราต้องการโปรแกรมภาษาอังกฤษที่ดีที่สุดแต่เราก็ต้องการความคุ้มค่าด้วย"',
+    classroom_teacher_review_des1: '- ครูใหญ่โรงเรียนลานนา -',
+    classroom_tab_des: 'เพื่อให้เหมาะสมกับหลักสูตรและวัฒนธรรมของโรงเรียน ซอฟต์แวร์ของเรา ถูกสร้างให้เราสามารถเขียนหลักสูตรที่เป็นของตัวเองได้ตามความต้องการ และตรงตามความสนใจของนักเรียน กิจกรรม เกมส์ และการแข่งขันได้ถูกรวมไว้ในหลักสูตรทั้งหมดของเรา เนื้อหาของบทเรียนที่มีชีวิตชีวาผ่านทางวิดีโอ เสียง และภาพที่น่าตื่นเต้น แบบฝึกหัดการพูดอ้างอิงตามการใช้งานจริงในชีวิตประจำวัน การอุ่นเครื่อง การเคลื่อนไหวของร่างกาย ซึ่งก็คือคุณจะได้ห้องเรียนแบบมีส่วนร่วมสูงสุดโดยที่ต้องใช้เวลาเตรียมตัวน้อยที่สุด <br/><br/> ถึงแม้ว่าซอฟต์แวร์ของเราจะเข้ากันได้อย่างดีกับการใช้ในห้องเรียนและอุปกรณ์แบบพกพาส่วนตัวของนักเรียนแล้ว มันยังสามารถใช้ประโยชน์ได้อย่างเต็มประสิทธิภาพ แม้ในสถานการณ์ที่มีทรัพยากรจำกัด หรือแม้แต่ใช้อุปกรณ์เพียงเครื่องเดียวก็ตาม<br/><br/> สามารถใช้เครื่องโปรเจคเตอร์ราคาไม่แพงในการนำกิจกรรมการเรียนการสอนของเราสอนทั้งชั้นเรียนได้ นอกจากนี้ ผู้เขียนบทเรียนของเรายังมีประสบการณ์มากมายในการทำงานโดยมีทรัพยากรที่จำกัด และสามารถช่วยให้ผู้เรียนบรรลุเป้าหมายทางภาษาโดยมีเพียงสิ่งของรอบตัวที่ใช้ในชีวิตประจำวันเท่านั้น<br/><br/> หากต้องการเรียนรู้เพิ่มเติมเกี่ยวกับบริการแบบทรัพยากรที่มีจำกัด โปรดดูต่อที่หน้าโครงการ <strong>“Levelling Up”</strong> ของเราได้',
+    // 
+    value_tab_heading: 'คุ้มค่าคุ้มราคา',
+    value_teacher_review_title1: '"เราต้องการโปรแกรมภาษาอังกฤษที่ดีที่สุดแต่เราก็ต้องการความคุ้มค่าด้วย"',
+    value_teacher_review_des1: '- ครูใหญ่โรงเรียนลานนา -',
+    value_tab_des: 'การประหยัดที่เห็นได้อย่างชัดเจน คือการกำจัดต้นทุนที่ไม่รู้จบจากการซื้อหนังสือเรียนอัพเดตใหม่และการเตรียมการสอบแบบใช้กระดาษ หลักสูตรและการทดสอบของเราทำขึ้นตามความต้องการของโรงเรียนและนักเรียน อีกทั้งยังสามารถนำมาใช้ซ้ำได้อีกเรื่อย ๆ โดยมีค่าใช้จ่ายเพิ่มเติมอีกเพียงเล็กน้อย หลักสูตรสามารถปรับเปลี่ยนได้ทุกเวลาในทุก ๆ ระดับ <br/><br/> สิ่งที่ระบบของเราช่วยให้องค์กรประหยัดต้นทุนได้มากขึ้น คือการลดเวลาที่ครูต้องใช้ในการเตรียมตัวและการให้คะแนน ด้วยกิจกรรมเสริมการเรียนที่สนุกสนาน ในทุกบทเรียน พร้อมทั้งระบบการให้คะแนนอัตโนมัติและการรายงานผลที่ใช้งานง่าย<br/><br/> ผลลัพธ์ที่ได้คือการยกระดับคุณภาพ ด้วยคอร์สเรียนดิจิทัลและรายงานที่บันทึกลงในอุปกรณ์ใด ๆ ก็ได้ รวมทั้งยังมีประสิทธิภาพที่เพิ่มขึ้นและการประหยัดอย่างเห็นได้ชัด',
+    // 
+    teaching_issues_heading: 'ปัญหาของการเรียน',
+    thai_tab_heading: 'การพัฒนาครู ชาวไทย',
+    thai_tab_heading_mo: 'การพัฒนาครูไทย',
+    thai_tab_teacher_review_title1: '" ฉันอยากจะพัฒนาภาษาอังกฤษของตัวเอง"',
+    thai_tab_teacher_review_des1: '- ครูใหญ่โรงเรียนลานนา -',
+    thai_tab_teacher_review_title2: '"ฉันอยากจะเรียนรู้เทคนิคการสอนมากกว่านี้"',
+    thai_tab_teacher_review_des2: '- ครูใหญ่โรงเรียนลานนา -',
+    thai_tab_des: 'หลักสูตรของเรามีข้อแนะนำที่ชัดเจนและง่ายในการปฏิบัติตามสำหรับครูชาวไทย ซอฟต์แวร์ของเรามีตัวช่วยในการออกเสียงตามหลักภาษาที่สำคัญทั้งหมด และมีคำแนะนำในการตั้งค่าซอฟต์แวร์ที่ครบครันด้วยบทเรียนมาพร้อมกับวิดีโอสอนและคำแปลภาษาไทย<br/><br/> ซอฟต์แวร์ของเราช่วยให้ครูชาวไทยสามารถพัฒนาความแม่นยำในการสอนของตนเอง พร้อมบ่งชี้ถีงข้อผิดพลาดที่มักเกิดขึ้นบ่อยครั้ง นอกจากนี้ ที่ปรึกษาของเรายังเป็นผู้ฝึกสอนครูที่มีประสบการณ์สูง และพร้อมให้บริการการช่วยเหลือแบบตรงจุด พร้อมการประเมินและข้อเสนอแนะ ทั้งแบบตัวต่อตัวหรือทางออนไลน์<br/><br/> วิธีนี้ช่วยให้ครูชาวไทยสามารถพิสูจน์ความสามารถในการสอนวิชาภาษาอังกฤษได้อย่างมีประสิทธิภาพตามแบบ หน่วยต่อหน่วย และบทต่อบท ด้วยความช่วยเหลือของเรา ครูชาวไทยไม่จำเป็นต้องพึ่งพาครูชาวต่างชาติเพื่อมอบประสบการณ์การเรียนรู้ที่มีคุณภาพสูงใหักับนักเรียน ช่วยให้ครูชาวไทยมีความมั่นใจและรู้สึกมีอิสระมากขึ้นในทุก ๆ หลักสูตรที่พวกเขาสอน',
+    // 
+    foreign_tab_heading: 'การพัฒนาครู ชาวต่างชาติ',
+    foreign_tab_heading_mo: 'การพัฒนาครูต่างชาติ',
+    foreign_tab_teacher_review_title1: '"เราอยากให้มีครูที่เป็นเจ้าของภาษา แต่ค่าจ้างมักมีราคาสูง "',
+    foreign_tab_teacher_review_des1: '- ครูใหญ่โรงเรียนลานนา -',
+    foreign_tab_teacher_review_title2: '"พนักงานชาวต่างชาติของเราเข้าๆ ออกๆ ค่อนข้างบ่อย"',
+    foreign_tab_teacher_review_des2: '- ครูใหญ่โรงเรียนลานนา -',
+    foreign_tab_teacher_review_title3: '"ฉันต้องการหาจุดสมดุลระหว่าง ครูชาวต่างชาติพูดภาษาอังกฤษได้ดีกว่า แต่ครูชาวไทยก็เข้าใจนักเรียนได้มากกว่า "',
+    foreign_tab_teacher_review_des3: '- ครูใหญ่โรงเรียนลานนา -',
+    foreign_tab_des: 'bla bla bla bla',
+    // thai_tab_heading_mo: 'cccc',
   },
 }
 
