3ec44cbaed68092187bbc4b23cbc4b1b9a7788ac
kondear-exelt <65880748+kondear-exelt@users.noreply.github.com>
2020-09-24 22:34:48 +0700

    active menu highlight

--- exelt/assets/js/active-menu-highlight.js
+++ exelt/assets/js/active-menu-highlight.js
@@ -1,7 +1,31 @@
-let currentURL = window.location.href;
+// let currentURL = window.location.href;
 
-document.querySelectorAll("#mobile-demo a").forEach(p => {
-  if(currentURL.indexOf(p.getAttribute("href")) !== -1){
-    p.classList.add("active");
-  }
-})
+// document.querySelectorAll("#mobile-demo a").forEach(p => {
+//   if(currentURL.indexOf(p.getAttribute("href")) !== -1){
+//     p.classList.add("active");
+//   }
+// })
+
+updateActiveMenu();
+
+$("#mobile-demo a").click(() => {
+  updateActiveMenu();
+});
+
+function updateActiveMenu() {
+  requestAnimationFrame(() => {
+    let currentURL = window.location.href;
+    let currentHash = currentURL.split("#")[1];
+
+    $("#mobile-demo a").each((k, dom) => {
+      const $el = $(dom);
+      let hash = $el.attr("href").split("#")[1];
+      let isActive =
+        currentURL.indexOf($el.attr("href")) !== -1 &&
+        currentHash === hash;
+
+      if (isActive) $el.addClass("active");
+      else $el.removeClass("active");
+    });
+  });
+}
--- kobfa/assets/js/dist/active-menu-highlight.dev.js
+++ kobfa/assets/js/dist/active-menu-highlight.dev.js
@@ -1,11 +1,5 @@
 "use strict";
 
-// let currentURL = window.location.href;
-// document.querySelectorAll("#mobile-demo a").forEach(p => {
-//   if(currentURL.indexOf(p.getAttribute("href")) !== -1){
-//     p.classList.add("active");
-//   }
-// })
 updateActiveMenu();
 $("#mobile-demo a").click(function () {
   updateActiveMenu();
