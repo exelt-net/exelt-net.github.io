2bd117071cbd91dd009c989f3bde13e1927351f4
Youngmin Wang <dev@exelt.net>
2023-07-11 13:30:34 +0700

    2023.07.09.02

--- kobfa/scripts/js/common.js
+++ kobfa/scripts/js/common.js
@@ -13,14 +13,14 @@ var contactLoaded = false;
 var bottomLoaded = false;
 
 $(document).ready(function (e) {
-  $("#xlt-header").load("./top.html?v=2023.07.09.02", null, () => {
+  $("#xlt-header").load("./top.html?v=2023.07.10.02", null, () => {
     topLoaded = true;
     if (topLoaded && bottomLoaded && contactLoaded) {
       onTopBottomReady();
     }
   });
   if ($("#xlt-contact").length > 0) {
-    $("#xlt-contact").load("./bottom-contacts.html?v=2023.07.09.02", null, () => {
+    $("#xlt-contact").load("./bottom-contacts.html?v=2023.07.10.02", null, () => {
       contactLoaded = true;
       if (topLoaded && bottomLoaded && contactLoaded) {
         onTopBottomReady();
@@ -29,7 +29,7 @@ $(document).ready(function (e) {
   } else {
     contactLoaded = true;
   }
-  $("#xlt-footer").load("./bottom.html?v=2023.07.09.02", null, () => {
+  $("#xlt-footer").load("./bottom.html?v=2023.07.10.02", null, () => {
     bottomLoaded = true;
     if (topLoaded && bottomLoaded && contactLoaded) {
       onTopBottomReady();
