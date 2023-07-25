f2d6b67a1b548e6a1a876efc82b2e6733ed212ed
Changhoon Park <hoon@exelt.net>
2023-07-25 19:45:21 +0900

    2023.07.25.08

--- kobfa/scripts/js/common.js
+++ kobfa/scripts/js/common.js
@@ -13,14 +13,14 @@ var contactLoaded = false;
 var bottomLoaded = false;
 
 $(document).ready(function (e) {
-  $("#xlt-header").load("./top.html?v=2023.07.25.07", null, () => {
+  $("#xlt-header").load("./top.html?v=2023.07.25.08", null, () => {
     topLoaded = true;
     if (topLoaded && bottomLoaded && contactLoaded) {
       onTopBottomReady();
     }
   });
   if ($("#xlt-contact").length > 0) {
-    $("#xlt-contact").load("./bottom-contacts.html?v=2023.07.25.07", null, () => {
+    $("#xlt-contact").load("./bottom-contacts.html?v=2023.07.25.08", null, () => {
       contactLoaded = true;
       if (topLoaded && bottomLoaded && contactLoaded) {
         onTopBottomReady();
@@ -29,7 +29,7 @@ $(document).ready(function (e) {
   } else {
     contactLoaded = true;
   }
-  $("#xlt-footer").load("./bottom.html?v=2023.07.25.07", null, () => {
+  $("#xlt-footer").load("./bottom.html?v=2023.07.25.08", null, () => {
     bottomLoaded = true;
     if (topLoaded && bottomLoaded && contactLoaded) {
       onTopBottomReady();
